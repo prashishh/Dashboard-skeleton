63c1a154837a4e2fe5fdea1db025812e588781ad
Rabi C Shah <green.rabi@hotmail.com>
2013-04-23 23:56:35 +0530

    Directive in the app now

--- js/directives.js
+++ js/directives.js
@@ -1,44 +1,14 @@
-
-
-
-
-
-
-
-/*
 // directives.js
-angular.module('demo.directives', [])
-    .directive('bar', function() {
-        return {
-            restrict: 'E',
-            scope: {
-              model: '='
-            },
-            link: function(scope, element, attrs, ctrl) {
-                element.text("Hello World!");
-               // var modelArray = scope.$eval(attrs.val[0]);
-              // attrs.model = ['1', '2'];
-               console.log(scope.model.values[1]);
-             // console.log(scope.);
-            }
-        };
-    });
+'use strict';
 
-
-angular.module('demo.directives', [])
-  .directive('bar', function() {
-    return {
-      restrict: 'A',
-      scope: {
-        text: "@myText",
-        twoWayBind: "=myTwoWayBind",
-        oneWayBind: "$myOneWayBind"
-      },
-      link: function(scope, element, attrs, ctrl) {
-          console.log(scope.text);
-      }
-    };
-  });
-
-  */
-
+angular.module('plunker')
+.directive('barDirective', function () {
+  return {
+    restrict : 'E',
+    scope : {
+      data: '=data'
+    },
+    template: '<div ng-repeat="li in data">{{li.val}} </div>',
+    replace: true
+  };
+});
