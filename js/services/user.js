aa21becde60e83555f0cf64b8ca126d495077d93
Prashish <prashish_raj@hotmail.com>
2013-05-30 21:34:12 +0530

    data binding between navig and setup controller

--- js/services/user.js
+++ js/services/user.js
@@ -2,10 +2,44 @@
 
 dashboardApp.factory('users', function() {
   var user = {
-    username: 'test',
-    password: 'test'
+    username: null,
+    password: null
   };
 
   return user;
-
 });
+
+dashboardApp.factory('credentials', function() {
+  
+  var cred = {
+    hostname: '1',
+    username: null,
+    password: null,
+    port: null
+  };
+
+  return {
+    getHostname: function() {
+        return cred.hostname;
+    },
+    setHostname: function(value) {
+      console.log(value);
+      cred.hostname = value;
+      console.log(value);
+    }
+  }
+
+/*
+ credService.getHostname = function() {
+    return cred.hostname;
+  };
+
+  credService.setHostname = function(value) {
+    cred.hostname = value;
+    console.log(cred.hostname);
+  };
+  */
+
+  return cred;
+});
+
